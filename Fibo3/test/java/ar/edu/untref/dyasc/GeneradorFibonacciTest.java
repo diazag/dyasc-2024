77b374941a3fb7b1306bb9e9fd7b7719938b49a5
Alan <ag.diaz.91@gmail.com>
2024-09-05 15:52:45 -0300

    Add files via upload

---
+++ Fibo3/test/java/ar/edu/untref/dyasc/GeneradorFibonacciTest.java
@@ -0,0 +1,81 @@
+package ar.edu.untref.dyasc;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Alan G. Diaz
+ **/
+
+public class GeneradorFibonacciTest {
+	
+	GeneradorFibonacci fibo =  new GeneradorFibonacci();
+	
+    @Test
+    public void testGenerarFibonacciZero() {
+    	int[] resultado = fibo.generarFibonacci(0);
+    	int[] esperado = {};
+        Assert.assertArrayEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testGenerarFibonacciUno() {
+    	int[] resultado = fibo.generarFibonacci(1);
+    	int[] esperado = {0};
+        Assert.assertArrayEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testGenerarFibonacciCinco() {
+    	int[] resultado = fibo.generarFibonacci(5);
+    	int[] esperado = {0,1,1,2,3};
+        Assert.assertArrayEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testGenerarFibonacciOcho() {
+    	int[] resultado = fibo.generarFibonacci(8);
+    	int[] esperado = {0,1,1,2,3,5,8,13};
+        Assert.assertArrayEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testGenerarFibonacciNegative() {
+    	int[] resultado = fibo.generarFibonacci(-3);
+    	int[] esperado = {};
+        Assert.assertArrayEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testInvertirSerie() {
+    	int[] resultado = fibo.generarFibonacci(5);
+    	int[] resultadoInvertido = fibo.invertirSerie(resultado);
+    	int[] esperado = {3,2,1,1,0};
+        Assert.assertArrayEquals(esperado, resultadoInvertido);
+    }
+    
+    @Test
+    public void testSumarFibonacci() {
+    	int[] serie = fibo.generarFibonacci(5);
+    	int resultado = fibo.sumarSerie(serie);
+    	int esperado = 7;
+        Assert.assertEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testSumarFibonacciZero() {
+    	int[] serie = fibo.generarFibonacci(0);
+    	int resultado = fibo.sumarSerie(serie);
+    	int esperado = 0;
+        Assert.assertEquals(esperado, resultado);
+    }
+    
+    @Test
+    public void testSumarFibonacciNegative() {
+    	int[] serie = fibo.generarFibonacci(-3);
+    	int resultado = fibo.sumarSerie(serie);
+    	int esperado = 0;
+        Assert.assertEquals(esperado, resultado);
+    }
+    
+}
